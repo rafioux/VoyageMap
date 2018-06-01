3f0aacb3b1027dfbb841c7c74941b6b74d0682be
AnjaTABARD <anja.tabard@etu.univ-lehavre.com>
2018-06-01 19:03:42 +0200

    Ajout du cycle de vie

--- app/src/main/java/com/example/rafioux/voyagemap/addDescription.java
+++ app/src/main/java/com/example/rafioux/voyagemap/addDescription.java
@@ -39,6 +39,10 @@ public class addDescription extends Activity {
         ajout();
     }
 
+    public void onPause() {
+        super.onPause();
+    }
+
     //fonction d'ajout
     public void ajout(){
         Intent monItent = getIntent();
@@ -104,66 +108,3 @@ public class addDescription extends Activity {
     }
 
 }
-
-
-/*package com.example.rafioux.voyagemap;
-
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-
-public class addDescription extends Activity {
-
-    EditText titre;
-    EditText commentaire;
-    String idVoyage;
-    String idLieux;
-    String lon;
-    String lat;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.adddescription);
-
-        //recuperation des informations
-        Intent monItent = getIntent();
-        idVoyage = monItent.getStringExtra("idVoyage");
-        idLieux = monItent.getStringExtra("idLieux");
-        lat = monItent.getStringExtra("lat");
-        lon = monItent.getStringExtra("lon");
-
-        //recuperation du lieu
-        LieuxBDD lieuxBdd = new LieuxBDD(this);
-        lieuxBdd.open();
-        Lieux l = lieuxBdd.getLieuxWithID(Integer.parseInt(idLieux));
-        lieuxBdd.close();
-
-        //si il existe deja des informations, on les met
-        if(l.getNom_lieux() != null || !l.getNom_lieux().equals("")){
-            titre = findViewById(R.id.titre);
-            titre.setText(l.getNom_lieux());
-        }
-
-        if(l.getCommentaire() != null || !l.getCommentaire().equals("")){
-            commentaire = findViewById(R.id.commentaire);
-            commentaire.setText(l.getCommentaire());
-        }
-    }
-
-    //sauvegarde les informations
-    public void save(View v){
-        titre = findViewById(R.id.titre);
-        commentaire = findViewById(R.id.commentaire);
-
-        LieuxBDD lieuxBdd = new LieuxBDD(this);
-        Lieux lieux = new Lieux(titre.getText().toString(), commentaire.getText().toString(),lon,lat,idVoyage);
-        lieuxBdd.open();
-        lieuxBdd.updateLieux(Integer.parseInt(idLieux),lieux);
-        lieuxBdd.close();
-
-        this.finish();
-    }
-}*/
